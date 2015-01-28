d5f3c893e8c6f7f838c80794d74d07db403bd5a0
Matheus <mths.ss@msn.com>
2015-01-28 16:35:32 -0200

    Setting the toolbar to replace the action bar

--- app/src/main/java/io/github/mths0x5f/guiaufu/ru/CardapioRUActivity.java
+++ app/src/main/java/io/github/mths0x5f/guiaufu/ru/CardapioRUActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -42,6 +43,8 @@ public class CardapioRUActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardapio_ru);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
